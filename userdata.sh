1c69a7628cdbf7e5031edadb22b7f25ec3cd3e3d
vamsi164 <vamsividavaluru@gmail.com>
2024-12-11 11:04:31 +0530

    Create userdata.sh

---
+++ userdata.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# Update system packages
+sudo yum update -y
+
+# Install Apache and start the service
+sudo yum install -y httpd
+sudo systemctl start httpd
+sudo systemctl enable httpd
+
+# Install MySQL client
+sudo yum install -y mysql
+
+# Install PHP and required extensions
+sudo amazon-linux-extras enable php7.2
+sudo yum install -y php php-mysqlnd
+
+# Download and configure WordPress
+cd /var/www/html
+wget https://wordpress.org/latest.zip
+unzip latest.zip
+mv wordpress/* .
+rm -rf wordpress latest.zip
+mv wp-config-sample.php wp-config.php
+
+# Configure wp-config.php
+sed -i "s/database_name_here/wordpress/" wp-config.php
+sed -i "s/username_here/harshath/" wp-config.php
+sed -i "s/password_here/harshath4891/" wp-config.php
+sed -i "s/localhost/wordpress.ct2qacwoeede.us-east-1.rds.amazonaws.com/" wp-config.php
+
+# Restart Apache to apply changes
+sudo systemctl restart httpd
