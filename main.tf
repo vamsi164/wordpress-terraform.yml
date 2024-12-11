0f9ab6c270af445e135438962423102a3055ad4a
vamsi164 <vamsividavaluru@gmail.com>
2024-12-11 11:02:57 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,47 @@
+resource "aws_instance" "wordpress" {
+  ami           = "ami-0166fe664262f664c" # Replace with the appropriate AMI for your region
+  instance_type = "t2.micro"             # Choose an instance type
+  
+  key_name      = "aws2"   # Replace with your existing key pair name
+  
+  user_data = file("userdata.sh")
+  
+  tags = {
+    Name = "WordPress-Instance"
+  }
+
+  security_groups = [aws_security_group.wordpress_sg.name]
+}
+
+resource "aws_security_group" "wordpress_sg" {
+  name        = "wordpress-sg"
+  description = "Allow HTTP, HTTPS, and SSH traffic"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
